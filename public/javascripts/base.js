68cc157ff74cb7e5f2c6ff6e997f12a64d116d73
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-10-01 00:06:22 +0900

    ズーム表示を複数ブラウザに対応

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,10 +1,17 @@
 $(function() {
   // 画面サイズを常に4:3にする
   $(window).bind('load orientationchange resize', function() {
-    $('html').css('zoom', Math.min(
-        window.innerWidth / document.body.clientWidth,
-        window.innerHeight / document.body.clientHeight
-      )
-    );
+    scale = Math.min(
+      window.innerWidth / document.body.clientWidth,
+      window.innerHeight / document.body.clientHeight
+    )
+    offest_left = ((document.body.clientWidth * scale) - document.body.clientWidth) / 2 / scale;
+    offest_top = ((document.body.clientHeight * scale) - document.body.clientHeight) / 2 / scale;
+    // for FireFox
+    $('body').css('-moz-transform', 'scale(' + scale + ') translate(' + offest_left + 'px, ' + offest_top + 'px)');
+    // for Opera
+    $('body').css('-o-transform', 'scale(' + scale + ') translate(' + offest_left + 'px, ' + offest_top + 'px)');
+    // for IE or WebKit
+    $('body').css('zoom', scale);
   });
 });
